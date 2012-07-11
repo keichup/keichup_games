6cf0df42441587ec4c3b4f7b196d79cfc8525155
Kei Onozawa <keichup@gmail.com>
2012-07-11 15:42:00 +0900

    io.test

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var express = require('express'),
-    app = express.createServer();
-
-app.get('/', function (req, res) {
-  res.send('Hello, World!');
-});
-
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+ 
 app.listen(process.env.PORT || 3000);
+ 
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+ 
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+ 
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
